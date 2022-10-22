42c079a07656d4500c7cda58e1b4c93ad6a7ec7e
Iori <ioifukifuku@gmail.com>
2022-10-22 23:12:25 +0000

    kadai23

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -117,7 +117,7 @@ return [
     'prohibited_unless' => 'The :attribute field is prohibited unless :other is in :values.',
     'prohibits' => 'The :attribute field prohibits :other from being present.',
     'regex' => 'The :attribute format is invalid.',
-    'required' => 'The :attribute field is required.',
+    'required' => ':attribute に入力が必要です。',
     'required_array_keys' => 'The :attribute field must contain entries for: :values.',
     'required_if' => 'The :attribute field is required when :other is :value.',
     'required_if_accepted' => 'The :attribute field is required when :other is accepted.',
@@ -169,6 +169,13 @@ return [
     |
     */
 
-    'attributes' => [],
+    'attributes' => [
+        'title' => 'タイトル',
+        'body' => '本文',
+        'name' =>'名前',
+        'gender' =>'性別',
+        'hobby' =>'趣味',
+        'introduction' =>'自己紹介'
+        ],
 
 ];
